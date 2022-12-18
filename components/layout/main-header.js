7e679719476d9906f704d34a88a264812ba4c920
saitamc <saritamc786@gmail.com>
2022-12-18 18:43:09 +1100

    next events app

---
+++ components/layout/main-header.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+import classes from './main-header.module.css';
+
+
+function MainHeader() {
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link href='/'>NextEvents</Link>
+      </div>
+      <nav className={classes.navigation}>
+        <Link href='/events'>Browse All Events</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default MainHeader;
